d1cf0e1b1053c5ae0247568b19b75f979a5218c9
Leo Davis <3577372+ldav1s@users.noreply.github.com>
2020-08-01 14:33:44 -0600

    Use OOP.  Clean up templates.

--- plugin.php
+++ plugin.php
@@ -1,10 +1,15 @@
 <?php
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 class Givinggrid_Widget extends WP_Widget {
 
   public function __construct() {
-    $widget_options = array( 'classname' => 'givinggrid_widget', 'description' => 'This is the Giving Grid Widget' );
-    parent::__construct( 'givinggrid_widget', 'Giving Grid Widget', $widget_options );
+    parent::__construct( 'givinggrid_widget',
+                         'Giving Grid Widget',
+                         array( 'classname' => 'givinggrid_widget',
+                                'description' => 'This is the Giving Grid Widget' ) );
+
+    add_action( 'wp_enqueue_scripts', array( $this, 'register_scripts' ) );
   }
 
   public function widget( $args, $instance ) {
@@ -13,25 +18,33 @@ class Givinggrid_Widget extends WP_Widget {
     $blog_title = get_bloginfo( 'name' );
     $tagline = get_bloginfo( 'description' );
 
-    echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title']; ?>
+    echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title'];
+    echo <<<"EOT"
 
-    <div id="givinggrid-widget" data-gid="<?php echo $givinggrid_id ?>" data-wol="0"></div>
-
-    <?php echo $args['after_widget'];
+    <div id="givinggrid-widget" data-gid="{$givinggrid_id}" data-wol="0"></div>
+EOT;
+    echo $args['after_widget'];
   }
 
   public function form( $instance ) {
     $title = ! empty( $instance['title'] ) ? $instance['title'] : '';
-    $givinggrid_id = ! empty( $instance['givinggrid_id'] ) ? $instance['givinggrid_id'] : ''; ?>
+    $givinggrid_id = ! empty( $instance['givinggrid_id'] ) ? $instance['givinggrid_id'] : '';
+    $attr_title = esc_attr( $title );
+    $title_id = $this->get_field_id( 'title' );
+    $title_name = $this->get_field_name( 'title' );
+    $gg_id = $this->get_field_id( 'givinggrid_id' );
+    $gg_id_name = $this->get_field_name( 'givinggrid_id' );
+    $attr_gg_id = esc_attr( $givinggrid_id );
+    echo <<<"EOT"
     <p>
-      <label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label> <br/>
-      <input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
+      <label for="{$title_id}">Title:</label> <br/>
+      <input type="text" id="{$title_id}" name="{$title_name}" value="{$attr_title}" />
     </p>
     <p>
-      <label for="<?php echo $this->get_field_id( 'givinggrid_id' ); ?>">Giving Grid ID:</label><br/>
-      <input type="number" id="<?php echo $this->get_field_id( 'givinggrid_id' ); ?>" name="<?php echo $this->get_field_name( 'givinggrid_id' ); ?>" value="<?php echo esc_attr( $givinggrid_id ); ?>" />
+      <label for="{$gg_id}">Giving Grid ID:</label><br/>
+      <input type="number" id="{$gg_id}" name="{$gg_id_name}" value="{$attr_gg_id}" />
     </p>
-    <?php
+EOT;
   }
 
   public function update( $new_instance, $old_instance ) {
@@ -41,18 +54,15 @@ class Givinggrid_Widget extends WP_Widget {
     return $instance;
   }
 
+  public function register_scripts() {
+      wp_register_script( 'givinggrid-js', 'https://www.givinggrid.com/g/gin.js', array(), null, true );
+      wp_enqueue_script( 'givinggrid-js' );
+  }
+
 }
 
-function gg_widget_enqueue_scripts() {
-
-    wp_register_script( 'givinggrid-js', 'https://www.givinggrid.com/g/gin.js', array(), null, true );
-    wp_enqueue_script( 'givinggrid-js' );
-}
-add_action( 'wp_enqueue_scripts', 'gg_widget_enqueue_scripts' );
-
-function register_givinggrid_widget() {
-  register_widget( 'Givinggrid_Widget' );
-}
-add_action( 'widgets_init', 'register_givinggrid_widget' );
+add_action( 'widgets_init', function() {
+    register_widget( 'Givinggrid_Widget' );
+});
 
 ?>
